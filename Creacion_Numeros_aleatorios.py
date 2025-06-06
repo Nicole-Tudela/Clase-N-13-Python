25a468a009aa27f700b67f7eb57dede2ca13cb95
Edna-Moda <ni.tudela@duocuc.cl>
2025-06-06 10:52:56 -0400

    Versión2
    Creación de números aleatorios versión 2

--- Creacion_Numeros_aleatorios.py
+++ Creacion_Numeros_aleatorios.py
@@ -1,5 +1,11 @@
-import random
+import random #Libreria para llamar y crear numeros
 
-numero = random.randint(1,100)
+numeros = [] #Coleccion vacia
 
-print (numero)
+for i in range(100): #Ciclo para repetir 100 veces las iteraciones
+  n = random.randint(1,100) #Crea el numero entre 1 y 100
+  numeros.append(random.randint(1,100)) # Guardamos en la lista el numero aleatorio
+
+
+print (numeros) #Visulaizamos lista
+
